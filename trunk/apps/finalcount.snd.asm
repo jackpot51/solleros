38570443d149ba437ed78747bfdff265926d156b
Jackpot51 <Jackpot51@064c71b8-074c-0410-837d-0302e1a0308f>
2009-09-04 19:57:33 +0000

    Updated finalcount.snd

--- trunk/apps/finalcount.snd.asm
+++ trunk/apps/finalcount.snd.asm
@@ -4,27 +4,74 @@ dd soundend
 	dw 30,B3
 	dw 100,C4#
 	dw 25,0
-	dw 100,F3#
-	dw 30,0
+	dw 150,F3#
+	
 	dw 35,D4
 	dw 35,C4#
 	dw 35,D4
 	dw 50,C4#
-	dw 100,B3
-	dw 30,0
+	dw 150,B3
+	
 	dw 30,D4
 	dw 30,C4#
 	dw 100,D4
 	dw 25,0
-	dw 100,F3#
-	dw 30,0
-	dw 30,B3
-	dw 30,A3
+	dw 150,F3#
+	
 	dw 35,B3
 	dw 35,A3
+	dw 40,B3
+	dw 40,A3
 	dw 50,G3#
 	dw 50,B3
-	dw 100,A3
+	dw 150,A3
 	
+	dw 30,C4#
+	dw 30,B3
+	dw 100,C4#
+	dw 150,F3#
+	
+	dw 35,D4
+	dw 35,C4#
+	dw 35,D4
+	dw 50,C4#
+	dw 150,B3
+	
+	dw 30,D4
+	dw 30,C4#
+	dw 100,D4
+	dw 150,F3#
+	dw 35,B3
+	dw 35,A3
+	dw 40,B3
+	dw 40,A3
+	dw 50,G3#
+	dw 50,B3
+	dw 70,A3
+	
+	dw 70,A3
+	dw 35,G3#
+	dw 35,A3
+	dw 80,B3
+	dw 35,A3
+	dw 35,B3
+	dw 80,C4#
+	dw 35,B3
+	dw 35,A3
+	dw 40,G3#
+	dw 50,F3#
+	
+	dw 70,D4
+	dw 150,C4#
+	dw 35,C4#
+	
+	dw 35,D4
+	dw 35,C4#
+	dw 35,B3
+	dw 150,C4#
+	
+	dw 30,B3
+	dw 30,G3#
+	dw 200,F3#
 soundend:
 %include 'music.asm'
