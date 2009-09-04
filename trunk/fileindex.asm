dccd1f5012ffb2b7d70335e838f0e81d6fa578e7
Jackpot51 <Jackpot51@064c71b8-074c-0410-837d-0302e1a0308f>
2009-09-04 01:43:53 +0000

    Fixed keycode, readded solleros.bmp.

--- trunk/fileindex.asm
+++ trunk/fileindex.asm
@@ -17,26 +17,29 @@ dd (f5-f4)/512
 db "piano",0
 dd (f5-$$)/512
 dd (f6-f5)/512
-db "solleros.txt",0
+db "solleros.bmp",0
 dd (f6-$$)/512
 dd (f7-f6)/512
-db "sound",0
+db "solleros.txt",0
 dd (f7-$$)/512
 dd (f8-f7)/512
-db "tely",0
+db "sound",0
 dd (f8-$$)/512
 dd (f9-f8)/512
-db "time",0
+db "tely",0
 dd (f9-$$)/512
 dd (f10-f9)/512
-db "tutorial.bat",0
+db "time",0
 dd (f10-$$)/512
 dd (f11-f10)/512
-db "unfs",0
+db "tutorial.bat",0
 dd (f11-$$)/512
 dd (f12-f11)/512
-db "unfs-system",0
+db "unfs",0
 dd (f12-$$)/512
 dd (f13-f12)/512
+db "unfs-system",0
+dd (f13-$$)/512
+dd (f14-f13)/512
 enddiskfileindex:
 
