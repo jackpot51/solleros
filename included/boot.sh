71912518af1d2872ee0bb382a01d31cf09e2af3d
Jackpot51 <Jackpot51@064c71b8-074c-0410-837d-0302e1a0308f>
2010-01-19 20:05:26 +0000

    I accidently the whole commit.

--- included/boot.sh
+++ included/boot.sh
@@ -2,8 +2,8 @@
 #11A=1280x1024
 #117=1024x768
 #111=640x480
-#exit #Comment this line out to automatically load the GUI
+exit #Comment this line out to automatically load the GUI
 gui 11A
-#playa victory.wav #Play asynchronously
+playa victory.wav #Play asynchronously
 background background.bmp#Load the background
 shush 10000 10000#Make the terminal full screen
