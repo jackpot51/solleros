30a8cd11c0b8b8498f5b3ae267256a0ebac06340
jackpot51@gmail.com <jackpot51@gmail.com@064c71b8-074c-0410-837d-0302e1a0308f>
2012-02-24 21:27:21 +0000

    Fixed booting of image. DOS compatible images cannot currently be booted

--- trunk/build/files.asm
+++ trunk/build/files.asm
@@ -3,48 +3,66 @@ f0:
 incbin "included/_img.bin"
 align 512,db 0
 f1:
-incbin "included/boot.sh"
+incbin "included/argtest.elf"
 align 512,db 0
 f2:
-incbin "included/dostest.com"
+incbin "included/boot.sh"
 align 512,db 0
 f3:
-incbin "included/exec"
+incbin "included/chartest.elf"
 align 512,db 0
 f4:
-incbin "included/finalcount.sn"
+incbin "included/dostest.com"
 align 512,db 0
 f5:
-incbin "included/fork"
+incbin "included/exec"
 align 512,db 0
 f6:
-incbin "included/guitest"
+incbin "included/finalcount.sn"
 align 512,db 0
 f7:
-incbin "included/int"
+incbin "included/fork"
 align 512,db 0
 f8:
-incbin "included/lostwoods.sn"
+incbin "included/guitest"
 align 512,db 0
 f9:
-incbin "included/mario.sn"
+incbin "included/int"
 align 512,db 0
 f10:
-incbin "included/raytrace.elf"
+incbin "included/lostwoods.sn"
 align 512,db 0
 f11:
-incbin "included/songotime.sn"
+incbin "included/mario.sn"
 align 512,db 0
 f12:
-incbin "included/sse"
+incbin "included/moment.wav"
 align 512,db 0
 f13:
-incbin "included/tely"
+incbin "included/pi.elf"
 align 512,db 0
 f14:
-incbin "included/tutorial.sh"
+incbin "included/raytrace.elf"
 align 512,db 0
 f15:
-incbin "included/unfs"
+incbin "included/songotime.sn"
 align 512,db 0
 f16:
+incbin "included/sse"
+align 512,db 0
+f17:
+incbin "included/stdiotest.elf"
+align 512,db 0
+f18:
+incbin "included/tely"
+align 512,db 0
+f19:
+incbin "included/timetest.elf"
+align 512,db 0
+f20:
+incbin "included/tutorial.sh"
+align 512,db 0
+f21:
+incbin "included/unfs"
+align 512,db 0
+f22:
