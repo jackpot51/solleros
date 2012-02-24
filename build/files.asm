15dd421f6902bb56a4f494bc256a932c700c06e2
jackpot51 <jackpot51@064c71b8-074c-0410-837d-0302e1a0308f>
2012-02-24 23:21:59 +0000

    Some stuff

--- build/files.asm
+++ build/files.asm
@@ -1,68 +1,68 @@
 align 512,db 0
 f0:
-incbin "included/_img.bin"
+incbin "included/tely"
 align 512,db 0
 f1:
-incbin "included/argtest.elf"
-align 512,db 0
-f2:
-incbin "included/boot.sh"
-align 512,db 0
-f3:
-incbin "included/chartest.elf"
-align 512,db 0
-f4:
 incbin "included/dostest.com"
 align 512,db 0
+f2:
+incbin "included/tutorial.sh"
+align 512,db 0
+f3:
+incbin "included/_img.bin"
+align 512,db 0
+f4:
+incbin "included/songotime.sn"
+align 512,db 0
 f5:
-incbin "included/exec"
+incbin "included/sse"
 align 512,db 0
 f6:
-incbin "included/finalcount.sn"
-align 512,db 0
-f7:
-incbin "included/fork"
-align 512,db 0
-f8:
 incbin "included/guitest"
 align 512,db 0
-f9:
-incbin "included/int"
+f7:
+incbin "included/mario.sn"
 align 512,db 0
-f10:
+f8:
 incbin "included/lostwoods.sn"
 align 512,db 0
+f9:
+incbin "included/exec"
+align 512,db 0
+f10:
+incbin "included/boot.sh"
+align 512,db 0
 f11:
-incbin "included/mario.sn"
+incbin "included/int"
 align 512,db 0
 f12:
 incbin "included/moment.wav"
 align 512,db 0
 f13:
-incbin "included/pi.elf"
-align 512,db 0
-f14:
-incbin "included/raytrace.elf"
-align 512,db 0
-f15:
-incbin "included/songotime.sn"
-align 512,db 0
-f16:
-incbin "included/sse"
-align 512,db 0
-f17:
-incbin "included/stdiotest.elf"
-align 512,db 0
-f18:
-incbin "included/tely"
-align 512,db 0
-f19:
 incbin "included/timetest.elf"
 align 512,db 0
+f14:
+incbin "included/pi.elf"
+align 512,db 0
+f15:
+incbin "included/raytrace.elf"
+align 512,db 0
+f16:
+incbin "included/fork"
+align 512,db 0
+f17:
+incbin "included/finalcount.sn"
+align 512,db 0
+f18:
+incbin "included/unfs"
+align 512,db 0
+f19:
+incbin "included/argtest.elf"
+align 512,db 0
 f20:
-incbin "included/tutorial.sh"
+incbin "included/chartest.elf"
 align 512,db 0
 f21:
-incbin "included/unfs"
+incbin "included/stdiotest.elf"
 align 512,db 0
 f22:
